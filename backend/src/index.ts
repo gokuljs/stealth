5c1c33c573201b8c2a0f714ef3f2adacc1af88e2
gokuljs <gokuljs029@gmail.com>
2024-06-07 04:13:56 +0530

    partial server setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 'use strict';
 import express from 'express';
+import http from 'http';
 import home from './Routes/HomeRouter';
 
 const app = express();
+const server = http.createServer(app);
 app.use(home);
 const port = 4000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
