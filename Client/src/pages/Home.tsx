802ab664c5b3af307928a1c17cbda42f93264a56
gokuljs <gokuljs029@gmail.com>
2024-06-08 08:31:25 +0530

    document

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -20,7 +20,7 @@ function Home(): JSX.Element {
             <TextEditor />
           ) : (
             <div className="flex items-center justify-center flex-col h-full w-full gap-4">
-              <div className="border rounded-full p-5 bg-stone-900">
+              <div className="border rounded-full p-5 bg-gray-100">
                 <img src="/create.svg" alt="create" className="h-[180px]" />
               </div>
               <Button onClick={onOpen}>Create New Document</Button>
