08d3126a39cb9e58564d9681f87a30a55014278b
gokuljs <gokuljs029@gmail.com>
2024-06-08 08:29:49 +0530

    empty document

--- Client/src/components/CreateDocumentModel.tsx
+++ Client/src/components/CreateDocumentModel.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
 import { useRef, useState } from 'react';
--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,18 +1,31 @@
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import TextEditor from '@/components/TextEditor/TextEditor';
+import { Button } from '@/components/ui/button';
 import ModelProvider from '@/providers/ModelProvider';
+import { useCreateDocumentModalStore } from '@/store /CreateDocumentModalStore';
 import { useParams } from 'react-router-dom';
 
 function Home(): JSX.Element {
   const { docId } = useParams();
+  const { onOpen } = useCreateDocumentModalStore();
+
   return (
     <>
       <div className="h-screen w-screen flex">
         <Sidebar />
         <div className="flex flex-col border flex-1">
           <Navbar />
-          {docId && <TextEditor />}
+          {docId ? (
+            <TextEditor />
+          ) : (
+            <div className="flex items-center justify-center flex-col h-full w-full gap-4">
+              <div className="border rounded-full p-5 bg-stone-900">
+                <img src="/create.svg" alt="create" className="h-[180px]" />
+              </div>
+              <Button onClick={onOpen}>Create New Document</Button>
+            </div>
+          )}
         </div>
       </div>
       <ModelProvider />
